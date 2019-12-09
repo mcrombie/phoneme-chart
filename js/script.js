ac9aa776fcc8a76b8f44297d40abcc0a9d33553a
Michael Crombie <mcrombie1994@gmail.com>
2019-12-08 17:00:39 -0800

    Uploading version 2. Introducing a new design, audio, buttons instead of radio buttons, tool tip text moved to the bottom right. In the next version: add a grid for readiability, find a way to make it clear to users the characters are clickable, use a nicer font, and play with different color schemes.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,22 @@
 /* VARIABLES */
 /* --------------------------------- */
 
-var vowels = document.getElementsByClassName('vowel');
-var ipaVowels = document.getElementsByClassName('ipa');
-var englishVowels = document.getElementsByClassName('english');
-var koreanVowels = document.getElementsByClassName('korean');
-var chineseVowels = document.getElementsByClassName('chinese');
-var radioButtons = ['ipa-radio', 'english-radio', 'chinese-radio'];
-
 var consonants = document.getElementsByClassName('consonant');
+var vowels = document.getElementsByClassName('vowel');
+
+var ipaPhonemes = document.getElementsByClassName('ipa');
+var englishPhonemes = document.getElementsByClassName('english');
+var chinesePhonemes = document.getElementsByClassName('chinese');
+
+var languages = ['ipa', 'english', 'chinese'];
+var phonemeCategories = ['consonants', 'vowels'];
+var optionsButtons = [];
+
+let buttonNormal = "#1a252f"; 
+let buttonHover = "#3e5771";
+let buttonSelected = "#4a6887";
+
+
 
 /* --------------------------------- */
 /* HIDE ALL VOWELS */
@@ -32,6 +40,28 @@ var hideConsonants = function(){
     
 }
 
+var phonemeDescription = function(html){
+    let displayText = html.getElementsByClassName("tooltiptext")[0].innerHTML;
+    document.getElementById("phoneme-description").innerHTML = displayText;
+
+}
+
+var activatePhoneme = function(element){
+    Array.prototype.forEach.call(document.getElementsByClassName("active"), function(element){
+        element.classList.remove("active");
+    })
+    element.className = "active phoneme tooltip"
+}
+
+var activateSpeaker = function(){
+    console.log("Speaker is Active!")
+    document.getElementById("speaker-icon").style="color:#2980b9;";
+    setTimeout(() => {
+        document.getElementById("speaker-icon").style="color:white;";
+    }, 1500);
+
+}
+
 /* --------------------------------- */
 /* DISPLAY SYMBOLS */
 /* --------------------------------- */
@@ -43,27 +73,52 @@ var display = function(){
     hideConsonants();
     
     
-    radioButtons.forEach(function(radioButton){
+    languages.forEach(function(language){
         
-        var language = [];
+        let phonemes = [];
         
+        let radioButton = language + "-radio";
+        let phonemeButton = language + "-button";
+
         //radio buttons map to a set of vowels
-        if(radioButton=='ipa-radio'){language=ipaVowels};
-        if(radioButton=='english-radio'){language=englishVowels};
-        if(radioButton=='korean-radio'){language=koreanVowels};
-        if(radioButton=='chinese-radio'){language=chineseVowels};
+        if(radioButton=='ipa-radio'){phonemes=ipaPhonemes};
+        if(radioButton=='english-radio'){phonemes=englishPhonemes};
+        if(radioButton=='chinese-radio'){phonemes=chinesePhonemes};
         
         //show or hide symbols based on radio button selection
         if(document.getElementById(radioButton).checked==true){
-            Array.prototype.forEach.call(language, function(element){
-
+            Array.prototype.forEach.call(phonemes, function(element){
                 element.style.display='block';
+                document.getElementById(phonemeButton).style.backgroundColor=buttonSelected;
             })
         }else{
-            Array.prototype.forEach.call(language, function(element){
+            Array.prototype.forEach.call(phonemes, function(element){
                 element.style.display='none';
+                document.getElementById(phonemeButton).style.backgroundColor=buttonNormal;
             })
         }
+    })
+
+    phonemeCategories.forEach(function(phonemeCategory){
+
+        let selectedPhonemeCategory;
+        
+        let radioButton = phonemeCategory + "-radio";
+        let phonemeCategoryButton = phonemeCategory + "-button";
+
+        if(radioButton=='vowels-radio'){selectedPhonemeCategory='vowels'};
+        if(radioButton=='consonants-radio'){selectedPhonemeCategory='consonants'};
+        
+
+        if(document.getElementById(radioButton).checked==true){
+            document.getElementById(selectedPhonemeCategory).style = "display:block;";
+            document.getElementById(phonemeCategoryButton).style.backgroundColor=buttonSelected;
+        }
+        else{
+            document.getElementById(selectedPhonemeCategory).style = "display:none;";
+            document.getElementById(phonemeCategoryButton).style.backgroundColor=buttonNormal;
+        }
+        
     })
     
 
